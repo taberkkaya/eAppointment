58b47b0d8552d0fc085e34bcc0fd7422262ed511
taberkkaya <taberkkaya@gmail.com>
2025-03-07 02:56:30 +0300

    create doctor mediator implementation

---
+++ eAppointmentServer/eAppointmentServer.Application/Feature/Doctors/CreateDoctor/CreateDoctorCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eAppointmentServer.Domain.Enums;
+using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Feature.Doctors.CreateDoctor;
+
+public sealed record CreateDoctorCommand(
+    string FirstName,
+    string LastName,
+    int Department) : IRequest<Result<string>>;
