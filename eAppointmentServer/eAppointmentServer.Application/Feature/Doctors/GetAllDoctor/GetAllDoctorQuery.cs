7ccf91811523032c8e1e7ce40c2fffae08ea64a7
taberkkaya <taberkkaya@gmail.com>
2025-03-07 02:56:52 +0300

    get all doctor mediator implementation

---
+++ eAppointmentServer/eAppointmentServer.Application/Feature/Doctors/GetAllDoctor/GetAllDoctorQuery.cs
@@ -0,0 +1,7 @@
+﻿using eAppointmentServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Feature.Doctors.GetAllDoctor;
+
+public sealed record GetAllDoctorQuery() : IRequest<Result<List<Doctor>>>;
