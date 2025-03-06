b0b91185c2289259c993df9bee57b00bb2dd272f
taberkkaya <taberkkaya@gmail.com>
2025-03-07 02:59:16 +0300

    add new route child to doctors page

--- eAppointmentClient/src/app/service/http.service.ts
+++ eAppointmentClient/src/app/service/http.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ResultModel } from '../models/result.model';
-import { api } from '../constant';
+import { api } from '../constants';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private error: ErrorService) {}
 
   post<T>(
     apiUrl: string,
@@ -17,9 +18,10 @@ export class HttpService {
   ) {
     this.http.post<ResultModel<T>>(`${api}/${apiUrl}`, body).subscribe({
       next: (res) => {
-        if (res.data !== undefined && res.data !== null) callBack(res);
+        callBack(res);
       },
       error: (err: HttpErrorResponse) => {
+        this.error.errorHandler(err);
         if (errCallBack !== undefined) {
           errCallBack(err);
         }
