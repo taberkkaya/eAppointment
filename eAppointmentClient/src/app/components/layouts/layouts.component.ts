a3b7bf4998bd77f3d2dd5662cd2a38963d7266b4
taberkkaya <taberkkaya@gmail.com>
2025-03-06 21:52:16 +0300

    login api consume done

--- eAppointmentClient/src/app/components/layouts/layouts.component.ts
+++ eAppointmentClient/src/app/components/layouts/layouts.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-layouts',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './layouts.component.html',
   styleUrl: './layouts.component.css',
 })
---
+++ eAppointmentClient/src/app/components/layouts/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  constructor(private router: Router) {}
+
+  signOut() {
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/login');
+  }
+}
