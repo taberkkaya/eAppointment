9ede1f4ac3fb3b02d0921412be8abed9180f20db
taberkkaya <taberkkaya@gmail.com>
2025-03-07 04:58:18 +0300

    patient pipe to searching

---
+++ eAppointmentClient/src/app/components/pipe/patient.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { PatientModel } from '../../models/patient.model';
+
+@Pipe({
+  name: 'patient',
+})
+export class PatientPipe implements PipeTransform {
+  transform(value: PatientModel[], search: string): PatientModel[] {
+    if (!search) return value;
+
+    return value.filter(
+      (p) =>
+        p.fullName.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        p.fullAddress
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase()) ||
+        p.city.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        p.town.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        p.identityNumber
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase())
+    );
+  }
+}
