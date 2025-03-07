b51232e813ee4c1ba0b304869ed32d26e975d6af
taberkkaya <taberkkaya@gmail.com>
2025-03-07 04:55:50 +0300

    update doctor cqrs

---
+++ eAppointmentServer/eAppointmentServer.Application/Feature/Doctors/UpdateDoctor/UpdateDoctorCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Feature.Doctors.UpdateDoctor;
+
+public sealed record UpdateDoctorCommand(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    int DepartmentValue) : IRequest<Result<string>>;
