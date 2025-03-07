de8f1cbfd50b2afe1c0937c5fd63da3789694df9
taberkkaya <taberkkaya@gmail.com>
2025-03-07 18:03:25 +0300

    create appointment cqrs

---
+++ eAppointmentServer/eAppointmentServer.Application/Feature/Appointments/CreateAppointment/CreateAppointmentCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Feature.Appointments.CreateAppointment
+{
+    public sealed record CreateAppointmentCommand(
+        string StartDate, 
+        string EndDate, 
+        Guid DoctorId,
+        Guid? PatientId,
+        string FirstName,
+        string LastName,
+        string IdentityNumber,
+        string City,
+        string Town,
+        string FullAddress): IRequest<Result<string>>;
+}
