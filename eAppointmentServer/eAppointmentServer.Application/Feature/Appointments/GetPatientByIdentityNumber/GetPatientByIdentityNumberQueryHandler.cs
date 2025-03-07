fa0ca3e83b8496770eb1e76fc58c4568705963f4
taberkkaya <taberkkaya@gmail.com>
2025-03-07 18:04:22 +0300

    get patient by id number cqrs

---
+++ eAppointmentServer/eAppointmentServer.Application/Feature/Appointments/GetPatientByIdentityNumber/GetPatientByIdentityNumberQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using eAppointmentServer.Domain.Entities;
+using eAppointmentServer.Domain.Repositories;
+using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Feature.Appointments.GetPatientByIdentityNumber
+{
+    internal sealed class GetPatientByIdentityNumberQueryHandler(IPatientRepository patientRepository) : IRequestHandler<GetPatientByIdentityNumberQuery, Result<Patient>>
+    {
+        public async Task<Result<Patient>> Handle(GetPatientByIdentityNumberQuery request, CancellationToken cancellationToken)
+        {
+            Patient? patient = await patientRepository
+                .GetByExpressionAsync(p => p.IdentityNumber == request.IdentityNumber,cancellationToken);
+            
+            return patient;
+        }
+    }
+}
