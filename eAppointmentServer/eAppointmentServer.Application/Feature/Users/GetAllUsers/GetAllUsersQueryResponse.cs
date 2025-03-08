b65d8a29e7c9573adcfa5a1fe2e8426b3e9f533c
taberkkaya <taberkkaya@gmail.com>
2025-03-08 04:50:46 +0300

    get all users cqrs

---
+++ eAppointmentServer/eAppointmentServer.Application/Feature/Users/GetAllUsers/GetAllUsersQueryResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace eAppointmentServer.Application.Feature.Users.GetAllUsers;
+
+public sealed record GetAllUsersQueryResponse()
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName {get; set;} = string.Empty;
+    public string? Email {get; set;}
+    public string? UserName {get; set;}
+    public List<string?> RoleIds { get; set; } = new();
+}
+
