ab194942f87cba3aafd465304c119b04f7c949ab
taberkkaya <taberkkaya@gmail.com>
2025-03-08 03:10:41 +0300

    constant for sync role

---
+++ eAppointmentServer/eAppointmentServer.Application/Constants.cs
@@ -0,0 +1,19 @@
+﻿using eAppointmentServer.Domain.Entities;
+
+namespace eAppointmentServer.Application;
+
+public static class Constants
+{
+    public static List<AppRole> GetRoles()
+    {   
+        List<string> roles = new()
+        {
+            "Admin",
+            "Doctor",
+            "Employee",
+            "Patient"
+        };
+
+        return roles.Select(s => new AppRole() { Name = s }).ToList();
+    }
+}
