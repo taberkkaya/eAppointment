8d9a673ad3a6b80c7f4ffaf83150b524f60b888f
taberkkaya <taberkkaya@gmail.com>
2025-03-08 03:08:19 +0300

    added auth token for requests

--- eAppointmentClient/src/app/service/http.service.ts
+++ eAppointmentClient/src/app/service/http.service.ts
@@ -8,7 +8,13 @@ import { ErrorService } from './error.service';
   providedIn: 'root',
 })
 export class HttpService {
-  constructor(private http: HttpClient, private error: ErrorService) {}
+  token: string = '';
+
+  constructor(private http: HttpClient, private error: ErrorService) {
+    if (localStorage.getItem('token')) {
+      this.token = localStorage.getItem('token') ?? '';
+    }
+  }
 
   post<T>(
     apiUrl: string,
@@ -16,16 +22,22 @@ export class HttpService {
     callBack: (res: ResultModel<T>) => void,
     errCallBack?: (err: HttpErrorResponse) => void
   ) {
-    this.http.post<ResultModel<T>>(`${api}/${apiUrl}`, body).subscribe({
-      next: (res) => {
-        callBack(res);
-      },
-      error: (err: HttpErrorResponse) => {
-        this.error.errorHandler(err);
-        if (errCallBack !== undefined) {
-          errCallBack(err);
-        }
-      },
-    });
+    this.http
+      .post<ResultModel<T>>(`${api}/${apiUrl}`, body, {
+        headers: {
+          Authorization: 'Bearer ' + this.token,
+        },
+      })
+      .subscribe({
+        next: (res) => {
+          callBack(res);
+        },
+        error: (err: HttpErrorResponse) => {
+          this.error.errorHandler(err);
+          if (errCallBack !== undefined) {
+            errCallBack(err);
+          }
+        },
+      });
   }
 }
