3f846cb272fdb3e4ba130f0b3557bb4e65559073
taberkkaya <taberkkaya@gmail.com>
2025-03-08 16:18:37 +0300

    user cqrs

---
+++ eAppointmentServer/eAppointmentServer.Application/Feature/Users/DeleteUserById/DeleteUserByIdCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using eAppointmentServer.Domain.Entities;
+using GenericRepository;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Feature.Users.DeleteUserById;
+
+internal sealed class DeleteUserByIdCommandHandler(
+    UserManager<AppUser> userManager,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteUserByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteUserByIdCommand request, CancellationToken cancellationToken)
+    {
+        AppUser? user = await userManager.FindByIdAsync(request.Id.ToString());
+        
+        if (user is null)
+            return Result<string>.Failure("User not found!");
+
+        IdentityResult result = await userManager.DeleteAsync(user);
+
+        if (!result.Succeeded)
+            return Result<string>.Failure(result.Errors.Select(s => s.Description).ToList());
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "User delete is successful!";
+    }
+}
+
--- eAppointmentServer/eAppointmentServer.Application/Mapping/MappingProfile.cs
+++ eAppointmentServer/eAppointmentServer.Application/Mapping/MappingProfile.cs
@@ -3,6 +3,8 @@ using eAppointmentServer.Application.Feature.Doctors.CreateDoctor;
 using eAppointmentServer.Application.Feature.Doctors.UpdateDoctor;
 using eAppointmentServer.Application.Feature.Patients.CreatePatient;
 using eAppointmentServer.Application.Feature.Patients.UpdatePatient;
+using eAppointmentServer.Application.Feature.Users.CreateUser;
+using eAppointmentServer.Application.Feature.Users.UpdateUser;
 using eAppointmentServer.Domain.Entities;
 using eAppointmentServer.Domain.Enums;
 
@@ -24,6 +26,10 @@ public sealed class MappingProfile : Profile
 
         CreateMap<CreatePatientCommand, Patient>();
         CreateMap<UpdatePatientCommand, Patient>();
+
+        CreateMap<CreateUserCommand, AppUser>();
+        CreateMap<UpdateUserCommand, AppUser>();
+        
 
     }
 }
